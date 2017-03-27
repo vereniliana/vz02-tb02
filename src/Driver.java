43d7e2e9cb4b1542a02a81b1d1364a5b3cf7e79c
sutantowilliam <willywilliamss97@gmail.com>
2017-03-28 01:12:25 +0700

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -19,28 +19,28 @@ public class Driver {
   /**
     * Membuat sebuah kebun binatang
     */
-  public static void CreateZoo(){
+  public static void createZoo(){
     try{
       Scanner inZoo = new Scanner(new File("zoo.txt"));
       Scanner inCage = new Scanner(new File("cage.txt"));
       Scanner inAnimal = new Scanner(new File("animal.txt"));
       Scanner in = new Scanner(System.in);
       myZoo = new Zoo();
-      // Read Zoo from file
-      myZoo.Read(inZoo);
-      // Read Cage from file and add to Zoo
+      // read Zoo from file
+      myZoo.read(inZoo);
+      // read Cage from file and add to Zoo
       int n_cage = inCage.nextInt();
       for(int i=0; i<n_cage; i++){
         Cage myCage = new Cage();
-        myCage.Read(inCage);
-        myZoo.AddCage(myCage);
+        myCage.read(inCage);
+        myZoo.addCage(myCage);
       }
-      // Read Animal from file and add to Zoo
+      // read Animal from file and add to Zoo
       int n_animal = inAnimal.nextInt();
       for(int i=0; i<n_animal; i++){
         Animal myAnimal = new Animal();
-        myAnimal.Read(inAnimal);
-        myZoo.AddAnimal(myAnimal);
+        myAnimal.read(inAnimal);
+        myZoo.addAnimal(myAnimal);
       }
       //inZoo.close();
       //inCage.close();
@@ -54,13 +54,13 @@ public class Driver {
   /**
     * Menampilkan daftar pilihan menu utama
     */
-  public static void ShowMenu(){
+  public static void showMenu(){
     System.out.println("Selamat datang di Kebun Binatang Virtual Zoo!");
     System.out.println("1. Menampilkan peta Kebun Binatang");
     System.out.println("2. Menampilkan seluruh peta pada Kebun Binatang");
     System.out.println("3. Menampilkan total makanan yang dimakan seluruh hewan");
-    System.out.println("4. Tour Kebun Binatang!");
-    System.out.println("5. Real Time Tour!");
+    System.out.println("4. tour Kebun Binatang!");
+    System.out.println("5. Real Time tour!");
     System.out.println("6. Quit");
     System.out.println("Pilih angka 1..6 :");
   }
@@ -69,28 +69,28 @@ public class Driver {
     */
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    CreateZoo();
+    createZoo();
     int n;
     do{
-      ShowMenu();
+      showMenu();
       n = in.nextInt();
       switch (n) {
         case 1:
           System.out.print("Input angka x1 y1 x2 y2 : ");
           int x1 = in.nextInt(), y1 = in.nextInt(), x2 = in.nextInt(), y2 = in.nextInt();
-          myZoo.Display(x1, y1, x2, y2);
+          myZoo.display(x1, y1, x2, y2);
           break;
         case 2:
-          myZoo.Display();
+          myZoo.display();
           break;
         case 3:
-          myZoo.PrintConsumedFood();
+          myZoo.printConsumedFood();
           break;
         case 4:
-          myZoo.Tour();
+          myZoo.tour();
           break;
         case 5:
-          myZoo.RealTimeTour();
+          myZoo.realTimetour();
           break;
         case 6:
           System.out.print("Thank You For Visiting Our Zoo");
